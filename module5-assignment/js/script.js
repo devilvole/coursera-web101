e60409ce389fd6537a713754aac626e4f84ff3a4
devilvole <machumph@yahoo.co.uk>
2019-05-31 17:01:31 +0100

    upd8 6 getting bored now

--- module5-assignment/js/script.js
+++ module5-assignment/js/script.js
@@ -123,7 +123,7 @@ function buildAndShowHomeHTML (categories) {
       var homeHtmlToInsertIntoMainPage = 
         insertProperty(homeHtml,
               "randomCategoryShortName",
-              '"' + chosenCategoryShortName + '"');
+              "'" + chosenCategoryShortName + "'");
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
