b69a26ba07ffc6b0630fe77c086e2c876312ca0c
devilvole <machumph@yahoo.co.uk>
2019-05-31 16:25:28 +0100

    upd8 1

--- module5-assignment/js/script.js
+++ module5-assignment/js/script.js
@@ -84,7 +84,7 @@ showLoading("#main-content");
   $ajaxUtils.sendGetRequest(
                             allCategoriesUrl,
                             function(allCategories){
-                              buildAndShowCategoriesHTML(allCategories)
+                              buildAndShowHomeHTML(allCategories)
                             }, 
                             true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
